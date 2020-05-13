62f9f69851d3b3aedd97335429a5e7a2467742da
thejadmaster <jeremy.driskell.web@gmail.com>
2020-05-12 18:16:42 -0600

    Initial commit for Vue project version.

---
+++ src/components/Switch.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="switch" aria-label="Switch Web Illustration Exercise">
+      <div class="controller left">
+        <div class="minus"></div>
+        <div class="joystick"></div>
+        <div class="dpad">
+          <div class="button up"></div>
+          <div class="button right"></div>
+          <div class="button down"></div>
+          <div class="button left"></div>
+        </div>
+        <div class="screenshot"></div>
+        <div class="bumper left"></div>
+      </div>
+
+      <div class="screen-container">
+        <div class="glass">
+          <div class="screen"></div>
+        </div>
+        <div class="volume left"></div>
+        <div class="volume right"></div>
+      </div>
+
+      <div class="controller right">
+        <div class="plus"></div>
+        <div class="button-container">
+          <div class="button x"></div>
+          <div class="button y"></div>
+          <div class="button a"></div>
+          <div class="button b"></div>
+        </div>
+        <div class="joystick"></div>
+        <div class="home"></div>
+        <div class="bumper right"></div>
+      </div>
+    </div>
+
+    <div class="switch-shadow"></div>
+  </div>
+</template>
+
+<script>
+module.exports = {};
+</script>
+
+<style lang="scss" src="../switch.scss"></style>
---
+++ src/switch.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+import SwitchIllustration from "./components/Switch.vue";
+import Hello from "./components/Hello.vue";
+
+new Vue({
+  el: "#switch-illustration",
+  components: { SwitchIllustration },
+  data: {},
+});
