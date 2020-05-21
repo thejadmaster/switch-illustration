339c5e296e6c201b179cc94be86775ecbdcd2976
thejadmaster <jeremy.driskell.web@gmail.com>
2020-05-21 12:38:48 -0600

    Create buttons

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -4,11 +4,17 @@
       <div class="controller left">
         <div class="minus"></div>
         <div class="joystick"></div>
-        <div class="dpad">
-          <div class="button up"></div>
-          <div class="button right"></div>
-          <div class="button down"></div>
-          <div class="button left"></div>
+        <div class="button-container">
+          <div class="row">
+            <div class="button up"><span></span></div>
+          </div>
+          <div class="row">
+            <div class="button left"><span></span></div>
+            <div class="button right"><span></span></div>
+          </div>
+          <div class="row">
+            <div class="button down"><span></span></div>
+          </div>
         </div>
         <div class="screenshot"></div>
         <div class="bumper left"></div>
@@ -23,12 +29,18 @@
       </div>
 
       <div class="controller right">
-        <div class="plus"></div>
+        <div class="plus-container"><div class="plus"></div></div>
         <div class="button-container">
-          <div class="button x"></div>
-          <div class="button y"></div>
-          <div class="button a"></div>
-          <div class="button b"></div>
+          <div class="row">
+            <div class="button x"><span>X</span></div>
+          </div>
+          <div class="row">
+            <div class="button y"><span>Y</span></div>
+            <div class="button a"><span>A</span></div>
+          </div>
+          <div class="row">
+            <div class="button b"><span>B</span></div>
+          </div>
         </div>
         <div class="joystick"></div>
         <div class="home"></div>
