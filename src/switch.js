bdf85dd4fb579dcfd12e7987be2cb9fe93fbb749
thejadmaster <jeremy.driskell.web@gmail.com>
2020-06-09 13:57:04 -0600

    Add TODOs for future work

--- src/switch.js
+++ src/switch.js
@@ -1,3 +1,9 @@
+/*
+ * Nintendo Switch Illustration
+ *
+ * TODO: Make it possible to change the lighting and controller colors using slide rules.
+ */
+
 import Vue from 'vue';
 import SwitchIllustration from './components/Switch.vue';
 
