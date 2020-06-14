b1ad8e13d72622d1b3142f94ef9a02e2e05b064a
thejadmaster <jeremy.driskell.web@gmail.com>
2020-06-14 12:32:30 -0600

    Update Buttons to address Safari error
    - Removed dynamic font in javascript - Animation of Switch hides FOUT on buttons

--- src/components/Buttons.vue
+++ src/components/Buttons.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="button-container">
     <div class="row">
-      <div v-bind:class="[isDpad ? arrowClass : letterClass]" class="button up">{{ upButton }}</div>
+      <div v-bind:class="[isDpad ? arrowClass : letterClass]" class="button up">
+        {{ upButton }}
+      </div>
     </div>
     <div class="row">
-      <div v-bind:class="[isDpad ? arrowClass : letterClass]" class="button left">{{ leftButton }}</div>
-      <div v-bind:class="[isDpad ? arrowClass : letterClass]" class="button right">{{ rightButton }}</div>
+      <div
+        v-bind:class="[isDpad ? arrowClass : letterClass]"
+        class="button left"
+      >
+        {{ leftButton }}
+      </div>
+      <div
+        v-bind:class="[isDpad ? arrowClass : letterClass]"
+        class="button right"
+      >
+        {{ rightButton }}
+      </div>
     </div>
     <div class="row">
-      <div v-bind:class="[isDpad ? arrowClass : letterClass]" class="button down">{{ downButton }}</div>
+      <div
+        v-bind:class="[isDpad ? arrowClass : letterClass]"
+        class="button down"
+      >
+        {{ downButton }}
+      </div>
     </div>
   </div>
 </template>
@@ -21,28 +38,19 @@ export default {
     right: String,
     down: String,
     left: String,
-    font: String,
-    fontUrl: String
   },
   data() {
     return {
-      upButton: "",
-      rightButton: "",
-      downButton: "",
-      leftButton: "",
-      arrowClass: "arrow",
-      letterClass: "letter"
+      upButton: '',
+      rightButton: '',
+      downButton: '',
+      leftButton: '',
+      arrowClass: 'arrow',
+      letterClass: 'letter',
     };
   },
-  created() {
-    // TODO: It didn't work! Figure out why!
-    if (
-      typeof this.font !== "undefined" &&
-      typeof this.fontUrl !== "undefined"
-    ) {
-      const font = new FontFace(this.font, this.fontUrl);
-      font.load().then(this.addButtonLetters());
-    }
+  mounted() {
+    this.addButtonLetters();
   },
   methods: {
     addButtonLetters() {
@@ -50,7 +58,7 @@ export default {
       this.rightButton = this.right;
       this.downButton = this.down;
       this.leftButton = this.left;
-    }
-  }
+    },
+  },
 };
 </script>
