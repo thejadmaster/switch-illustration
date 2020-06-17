5ff9b9562c3afdfa6c8941e90847f06bf38c74d5
thejadmaster <jeremy.driskell.web@gmail.com>
2020-06-16 23:29:04 -0600

    Created optimized versions of the images
    - Used srcset to switch assets based on the requested image quality.

--- src/components/LogoContainer.vue
+++ src/components/LogoContainer.vue
@@ -4,7 +4,8 @@
       <div ref="sass" class="sass logo-container">
         <img
           class="sass-logo logo"
-          src="img/Sass-Logo_Black.png"
+          srcset="img/Sass-Logo_Black_2x.png 2x, img/Sass-Logo_Black_3x.png 3x"
+          src="img/Sass-Logo_Black_1x.png"
           alt="Sass Homepage"
         />
         <font-awesome-icon class="my-icon" icon="external-link" />
@@ -17,12 +18,14 @@
       <div ref="github" class="github logo-container">
         <img
           class="github-logo logo"
-          src="img/GitHub_Logo.png"
+          srcset="img/GitHub_Logo_2x.png 2x, img/GitHub_Logo_3x.png 3x"
+          src="img/GitHub_Logo_1x.png"
           alt="Switch Illustration GitHub Repository"
         />
         <img
           class="github-octocat logo"
-          src="img/Octocat.png"
+          srcset="img/Octocat_2x.png 2x, img/Octocat_3x.png 3x"
+          src="img/Octocat_1x.png"
           alt="Switch Illustration GitHub Repository"
         />
         <font-awesome-icon class="my-icon" icon="external-link" />
@@ -30,7 +33,12 @@
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <div ref="vue" class="vue-logo-container logo-container">
-        <img class="logo" src="img/vue_logo.png" alt="VueJS Homepage" />
+        <img
+          class="logo"
+          srcset="img/vue_logo_2x.png 2x, img/vue_logo_3x.png 3x"
+          src="img/vue_logo_1x.png"
+          alt="VueJS Homepage"
+        />
         <div class="vue-text">Vue.js</div>
         <font-awesome-icon class="my-icon" icon="external-link" />
       </div>
